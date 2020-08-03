d810c4698bc409887a3b069d9f38571687e28ac2
rahulrao <rahuldrao21@gmail.com>
2020-08-03 14:20:20 -0400

    Initial check-in of module examPrep

---
+++ examPrep/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+
+public class QuadraticEquation
+{
+    static int minimumbox(int []arr, int n)
+    {
+        Queue<int> q = new Queue<int>();
+
+        Array.Sort(arr);
+        int a;
+        q.Enqueue(arr[0]);
+        for(int i=0;i<n;i++)
+        {
+            a = q.Peek();
+            if(arr[i]>(2*a))
+            {
+                q.Dequeue();
+                q.Enqueue(arr[i]);
+            }
+        }
+
+        return q.Count;
+    }
+
+    public static void Main(string[] args)
+    {
+        int[] arr = { 4, 1, 2, 8 };
+        int n = arr.Length;
+
+        Console.WriteLine(minimumbox(arr, n));
+    }
+}
